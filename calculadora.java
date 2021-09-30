06b5a44603b35dd0692898fcf9df37ea2409c888
nab <66952624+nabplhres@users.noreply.github.com>
2021-09-29 22:41:47 -0300

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -1,44 +1,46 @@
-package br.com.dio.calculadora;
+package  br.com.dio.calculadora ;
 
-import java.util.Scanner;
+import  java.util.Scanner ;
 
-public class calculadora {
+public  class  Calculadora {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
+	public  static  void  main ( String [] args ) {
+		Varredura do scanner =  novo  scanner ( sistema . In);
 		
-			int a,b;
+		int a, b;
 		
-			System.out.println("digite o primeiro valor");
-			a = scan.nextInt();
-			System.out.println("digite o segundo valor");
-			b = scan.nextInt();
-			
-			int soma = soma(a,b);
-			int subtracao = subtracao(a,b);
-			int multiplicacao = multiplicacao(a,b);
-			int divisao = divisao(a,b);
-			
-			System.out.println(soma);
-			System.out.println(subtracao);
-			System.out.println(divisao);
-			System.out.println(multiplicacao);
+		Sistema . para fora . println ( " Digite o primeiro valor: " );
+		a = scan . nextInt ();
+		Sistema . para fora . println ( " Digite o segundo valor: " );
+		b = digitalizar . nextInt ();
+		
+		int soma = soma (a, b);
+		int subtracao = subtracao (a, b);
+		int multiplicacao = multiplicacao (a, b);
+		int divisao = divisao (a, b);
+		
+		Sistema . para fora . println ( " soma "  + soma);
+		Sistema . para fora . println ( " sub "  + subtração);
+		Sistema . para fora . println ( " mult "  + multiplicação);
+		Sistema . para fora . println ( " div "  + divisao);
+		
 	}
 	
+	public  static  int  soma ( int  a , int  b ) {
+		retornar a + b;
+	}
+	
+	public  static  int  subtracao ( int  a , int  b ) {
+		retornar a - b;
+	}
+	
+	public  static  int  divisao ( int  a , int  b ) {
+		return a / b;
+	}
+	
+	public  static  int  multiplicacao ( int  a , int  b ) {
+		return a * b;
+	}
+	
+
 }
-	
-	public static int soma(int a, b) {
-		return a+b;
-	}
-	public static int subtracao(int a, b) {
-		return a-b;
-	}
-	public static int divisao(int a, b) {
-		return a/b;
-	}
-	public static int multiplicacao(int a, b) {
-		return a*b;
-	}
-
-
